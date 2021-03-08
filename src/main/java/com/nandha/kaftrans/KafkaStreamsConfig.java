c39758a5a58335b142c4fdeb5e8c436f7ce17a1f
Nandha Kumar Viswanaathan <nandha.viswanathan@sifycorp.com>
2021-03-08 12:57:13 +0530

    unused imports removed

--- src/main/java/com/nandha/kaftrans/KafkaStreamsConfig.java
+++ src/main/java/com/nandha/kaftrans/KafkaStreamsConfig.java
@@ -4,8 +4,6 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
 
@@ -13,8 +11,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@EnableKafka
-@EnableKafkaStreams
 public class KafkaStreamsConfig {
 
     @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
