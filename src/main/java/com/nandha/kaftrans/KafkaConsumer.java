00f0b7bd71a93f380e61fcd59eeb88616a2b5706
Nandha Kumar Viswanaathan <nandhakumarviswanaathan@gmail.com>
2021-03-10 23:04:52 +0530

    kafka consumer example added

---
+++ src/main/java/com/nandha/kaftrans/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package com.nandha.kaftrans;
+
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@EnableKafka
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "user_out")
+    public void processMessage(String content) {
+
+        System.out.println("\n--------------------------------[ nandha ]---------------------------------\n");
+        System.out.println(content);
+        System.out.println("\n--------------------------------[ nandha ]---------------------------------\n");
+
+    }
+}
