ab361bf90e223e2174edc12e603834e79ac72e49
Nandha Kumar Viswanaathan <nandhakumarviswanaathan@gmail.com>
2021-03-11 18:55:02 +0530

    new fields introduced

--- src/main/java/com/nandha/kaftrans/User.java
+++ src/main/java/com/nandha/kaftrans/User.java
@@ -1,6 +1,8 @@
 package com.nandha.kaftrans;
 
 public class User {
+    public String firstname;
+    public String lastname;
     public String username;
     public String email;
     public int age;
