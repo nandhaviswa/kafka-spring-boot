b18622313502bc8ca99cd6044d83f35b345886e5
Nandha Kumar Viswanaathan <nandhakumarviswanaathan@gmail.com>
2021-03-11 18:53:24 +0530

    unused imports removed, infrastructure di done

--- src/main/java/com/nandha/kaftrans/KafkaStreamsExample.java
+++ src/main/java/com/nandha/kaftrans/KafkaStreamsExample.java
@@ -1,22 +1,14 @@
 package com.nandha.kaftrans;
 
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Produced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
-import org.springframework.kafka.support.serializer.JsonSerde;
 
 @Configuration(proxyBeanMethods = false)
 @EnableKafkaStreams
 public class KafkaStreamsExample {
     
-    public KafkaStreamsExample(StreamsBuilderFactoryBean myKStreamBuilderFactoryBean){
-        myKStreamBuilderFactoryBean.setInfrastructureCustomizer(new NndaInfrastructure());
+    public KafkaStreamsExample(StreamsBuilderFactoryBean myKStreamBuilderFactoryBean, NndaInfrastructure infrastructure){
+        myKStreamBuilderFactoryBean.setInfrastructureCustomizer(infrastructure);
     }
 }
